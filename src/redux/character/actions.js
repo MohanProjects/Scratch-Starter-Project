098c7b02fb86c8dc8d0e8c043570c9647916cbf5
mohan1218 <68373515+MohanProjects@users.noreply.github.com>
2024-09-08 21:12:46 +0530

    Update actions.js

--- src/redux/character/actions.js
+++ src/redux/character/actions.js
@@ -1,5 +1,5 @@
+//ADD Characters
 import { ADD_CHARACTER, SET_ACTIVE_CHARACTER, SET_ANGLE } from "./actionTypes";
-
 export const setCharacterAngle = (characterAngle) => {
   return {
     type: SET_ANGLE,
